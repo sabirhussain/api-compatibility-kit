57be69d4c1d6c48316fc0585240fbe8d9d5548f4
Sabir Hussain <Sabir.Hussain@wu.com>
2026-09-01 17:08:58 +0530

    Advance configuration option provided for better extended use of the kit

--- api-compatibility-kit-spring-webmvc/src/main/java/io/xprevel/util/api/spring/ApiVersionFilter.java
+++ api-compatibility-kit-spring-webmvc/src/main/java/io/xprevel/util/api/spring/ApiVersionFilter.java
@@ -1,7 +1,6 @@
 package io.xprevel.util.api.spring;
 
-import io.xprevel.util.api.ApiVersionContext;
-import io.xprevel.util.api.ApiVersionResolver;
+import io.xprevel.util.api.VersionResolver;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,22 +8,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 
-public class ApiVersionFilter<E extends Enum<E>> extends OncePerRequestFilter {
-    private final String headerName;
-    private final ApiVersionResolver<E> resolver;
-    private final ApiVersionContext<E> context;
+public final class ApiVersionFilter<E extends Enum<E>> extends OncePerRequestFilter {
+    private final VersionResolver<E, HttpServletRequest> resolver;
+    private final Consumer<E> versionConsumer;
 
-    public ApiVersionFilter(ApiVersionResolver<E> resolver, ApiVersionContext<E> context, String headerName) {
+    public ApiVersionFilter(VersionResolver<E, HttpServletRequest> resolver, Consumer<E> versionConsumer) {
         this.resolver = resolver;
-        this.context = context;
-        this.headerName = headerName;
+        this.versionConsumer = versionConsumer;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        E apiVersion = resolver.resolve(request.getHeader(headerName));
-        context.setVersion(apiVersion);
+        E apiVersion = resolver.resolve(request);
+        versionConsumer.accept(apiVersion);
         filterChain.doFilter(request, response);
     }
 }
